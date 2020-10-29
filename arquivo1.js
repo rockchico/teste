faf956dc0314546aa7e30b53c548dffecbbafadb
Francisco Schwertner <franciscos@universo.univates.br>
2020-10-29 09:48:54 -0300

    adicionei a sexta funcionalidade

--- arquivo1.js
+++ arquivo1.js
@@ -2,4 +2,5 @@ console.log('feature 1');
 console.log('feature 2 123');
 console.log('feature 3 123');
 console.log('feature 4 1234');
-console.log('FEATURE 5')
+console.log('FEATURE 5');
+console.log('feature 6');
